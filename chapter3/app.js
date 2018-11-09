d6627fc337aa1032348ea2378ac153bdeba0c2ba
Wataru.Kasahara <kasahara1019@gmail.com>
2018-11-09 09:23:21 +0900

    フルーツ一覧の説明追加

--- chapter3/app.js
+++ chapter3/app.js
@@ -2,6 +2,10 @@ Vue.component('fruits-list-title', {
   template: '<h1>フルーツ一覧</h1>',
 });
 
+Vue.component('fruits-list-description', {
+  template: '<p>季節の代表的なフルーツの一覧です</p>',
+});
+
 new Vue({
   el: '#fruits-list',
 });
