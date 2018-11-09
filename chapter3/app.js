5f552c27e1841d9da0711aeb1f96fd087f34b1b1
Wataru.Kasahara <kasahara1019@gmail.com>
2018-11-09 09:28:31 +0900

    フルーツ一覧の表を追加

--- chapter3/app.js
+++ chapter3/app.js
@@ -6,6 +6,33 @@ Vue.component('fruits-list-description', {
   template: '<p>季節の代表的なフルーツの一覧です</p>',
 });
 
+Vue.component('fruits-list-table', {
+  template: `
+    <table>
+      <tr>
+        <th>季節</th>
+        <th>フルーツ</th>
+      </tr>
+      <tr>
+        <td>春</td>
+        <td>いちご</td>
+      </tr>
+      <tr>
+        <td>夏</td>
+        <td>すいか</td>
+      </tr>
+      <tr>
+        <td>秋</td>
+        <td>ぶどう</td>
+      </tr>
+      <tr>
+        <td>冬</td>
+        <td>みかん</td>
+      </tr>
+    </table>
+  `,
+});
+
 new Vue({
   el: '#fruits-list',
 });
