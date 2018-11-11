832ae1d1c3f97b6c77b50de3159f70ecc88e7491
Wataru.Kasahara <kasahara1019@gmail.com>
2018-11-11 21:13:30 +0900

    描画関数を使った日付の表示

--- chapter3/app.js
+++ chapter3/app.js
@@ -47,6 +47,20 @@ Vue.component('fruits-list-title', FruitsListTitle);
 Vue.component('fruits-list-description', FruitsListDescription);
 Vue.component('fruits-list-table', FruitsListTable);
 
+Vue.component('input-date-with-today', {
+  render: function(createElement) {
+    return createElement(
+      'input',
+      {
+        attrs: {
+          type: 'date',
+          value: new Date().toISOString().substring(0, 10)
+        }
+      }
+    );
+  }
+});
+
 new Vue({
   el: '#main',
   components: {
