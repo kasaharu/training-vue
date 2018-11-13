03fffdd8fa67628be9de9ccf18aff344a73ef374
Wataru.Kasahara <kasahara1019@gmail.com>
2018-11-13 09:31:08 +0900

    props を利用して子コンポーネントにデータを伝搬

--- chapter3/app.js
+++ chapter3/app.js
@@ -78,8 +78,22 @@ new Vue({
   data: { myItem: 'pen' },
 });
 
+Vue.component('fruits-item-name', {
+  props: {
+    fruitsItem: {
+      type: Object,
+      required: true,
+    },
+  },
+  template: '<li>{{fruitsItem.name}}</li>',
+});
+
 new Vue({
   el: '#fruits-component',
   data: {
+    fruitsItems: [
+      { name: '梨' },
+      { name: 'イチゴ' },
+    ],
   },
 });
