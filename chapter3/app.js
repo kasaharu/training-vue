0e0252e8209ef77eb1c655ae99c903dc2c985d40
Wataru.Kasahara <kasahara1019@gmail.com>
2018-11-14 09:33:55 +0900

    ボタンを表示する fruits-counter コンポーネントを用意

--- chapter3/app.js
+++ chapter3/app.js
@@ -97,3 +97,31 @@ new Vue({
     ],
   },
 });
+
+var counterButton = Vue.extend({
+  template: '<span>{{counter}}個<button v-on:click="addToCart">追加</button></span>',
+  data: function() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    addToCart: function() {
+      this.counter += 1;
+      this.$emit('increment')
+    },
+  },
+});
+
+new Vue({
+  el: '#fruits-counter',
+  components: {
+    'counter-button': counterButton,
+  },
+  data: {
+    fruits: [
+      { name: '梨' },
+      { name: 'イチゴ' },
+    ],
+  },
+});
