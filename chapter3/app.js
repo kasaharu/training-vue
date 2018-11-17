c79fc0c2c11055f5f26c3f4a7c1bde5e640086d1
Wataru.Kasahara <kasahara1019@gmail.com>
2018-11-17 14:31:22 +0900

    ヘッダーコンポーネントの作成

--- chapter3/app.js
+++ chapter3/app.js
@@ -131,3 +131,17 @@ new Vue({
     }
   },
 });
+
+var headerTemplate = `
+  <div style="color: gray;">
+    <slot name="header">※親から何も渡ってこない場合、この文が表示されます。</slot>
+  </div>
+`;
+
+Vue.component('page-header', {
+  template: headerTemplate,
+});
+
+new Vue({
+  el: "#fruits-list",
+});
