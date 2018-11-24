f2134a8b015c71d8442d06c89752960de42cb62f
Wataru.Kasahara <kasahara1019@gmail.com>
2018-11-24 09:02:21 +0900

    page-content の用意

--- chapter3/app.js
+++ chapter3/app.js
@@ -133,13 +133,22 @@ new Vue({
 });
 
 var headerTemplate = `
-  <div style="color: gray;">
-    <slot name="header">※親から何も渡ってこない場合、この文が表示されます。</slot>
+  <div>
+    <slot name="header">No title</slot>
+  </div>
+`;
+
+var contentTemplate = `
+  <div>
+    <slot name="content">No contents</slot>
   </div>
 `;
 
 Vue.component('page-header', {
   template: headerTemplate,
+});
+Vue.component('page-content', {
+  template: contentTemplate,
 });
 
 new Vue({
